7806ad5a3bc1cd6f6864cc7292faf582664c305e
Alex Pina <alexpina16@msn.com>
2020-03-10 20:07:29 -0700

    update button working

--- src/components/dashboard/RecipeEdit.js
+++ src/components/dashboard/RecipeEdit.js
@@ -16,8 +16,9 @@ const RecipeEdit = (props) => {
     const recipe = recipes.filter(recipe => recipe.id === Number(params.id))
     const dispatch = useDispatch();
 
-    const { recipeName, hackNeeded, ingredients, preparation, cookingInstructions, hack } = recipe[0]
-    const [state, setState] = useState({
+    const { id, recipeName, hackNeeded, ingredients, preparation, cookingInstructions, hack } = recipe[0]
+  const [state, setState] = useState({
+        id: id,
         recipeName: recipeName,
         hackNeeded: hackNeeded,
         ingredients: ingredients,
@@ -33,6 +34,16 @@ const RecipeEdit = (props) => {
         //setTimeout(window.location.reload(), 1000)
     };    
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    dispatch(updateRecipe(state, props.history.push, state.id ))
+  }
+  const handleChange = name => e => {
+    setState({
+      ...state,
+      [name]: e.target.value
+    })
+  }
 
 return (
   <div>
@@ -46,34 +57,40 @@ return (
         <img width="100%" src={Hands} alt=" " />  
       </Col>
       <Col sm="8">
-        <Form>
+        <Form onSubmit={handleSubmit}>
             <FormGroup>
                 <Label for="recipeName">Recipe Name</Label>
-                            <Input type="textArea" name="recipeName" id="recipeName" defaultValue={state.recipeName}/>
+              <Input type="textArea" name="recipeName" id="recipeName" defaultValue={state.recipeName}
+              onChange={handleChange("recipeName")}/>
             </FormGroup>
             <FormGroup>
                 <Label for="hackNeeded">Requested Hack</Label>
-                    <Input type="textArea" name="hackNeeded" id="hackNeeded" defaultValue={state.hackNeeded}/>
+              <Input type="textArea" name="hackNeeded" id="hackNeeded" defaultValue={state.hackNeeded}
+              onChange={handleChange("hackNeeded")}/>
             </FormGroup>                 
             <FormGroup>
                 <Label for="ingredients">Ingredients</Label>
-                    <Input type="textArea" name="ingredients" id="ingredients" defaultValue={state.ingredients}/>
+              <Input type="textArea" name="ingredients" id="ingredients" defaultValue={state.ingredients}
+              onChange={handleChange("ingredients")}/>
             </FormGroup>                 
             <FormGroup>
                 <Label for="preparationInstructions">Preparation Instructions</Label>
-                    <Input type="textArea" name="preparationInstructions" id="preparationInstructions" defaultValue={state.preparation}/>
+              <Input type="textArea" name="preparationInstructions" id="preparationInstructions" defaultValue={state.preparation}
+                onChange={handleChange("preparation")}/>
             </FormGroup>  
             <FormGroup>
                 <Label for="cookingInstructions">Cooking Instructions</Label>
-                    <Input type="textArea" name="cookingInstructions" id="cookingInstructions" defaultValue={state.cookingInstructions}/>
+              <Input type="textArea" name="cookingInstructions" id="cookingInstructions" defaultValue={state.cookingInstructions}
+                onChange={handleChange("cookingInstructions")} />
             </FormGroup>  
             <FormGroup>
                <Label for="hack">Hack</Label>
-                    <Input type="textArea" name="hack" id="hack" defaultValue={state.hack}/>
+              <Input type="textArea" name="hack" id="hack" defaultValue={state.hack}
+              onChange={handleChange("hack")}/>
             </FormGroup>
-            <Link to="/recipelist"> 
-              <Button><h4>Submit Edit</h4></Button>
-            </Link>
+            
+              <Button type="submit"><h4>Submit Edit</h4></Button>
+          
             <Link to="/recipelist">
                 <Button><h4>Cancel</h4></Button>
             </Link>
